2e787a1ab52bd8329013b7127a982b0594ffeb51
mirantel <magi.mirantel@gmail.com>
2014-04-01 14:56:12 +0300

    Fixed class name in js

--- production/js/common.js
+++ production/js/common.js
@@ -1,11 +1,11 @@
 $(window).load(function(){
 
-$("#voite, .voit-cell").sortable({
+$("#voite, .voit__cell").sortable({
   placeholder: "ui-sortable-placeholder",
   connectWith: ".connectedSortable",
   receive: function(event, ui) {
 		var $this = $(this);
-		if ($this.children('.voit-item').length > 1 && $this.attr('id') != "voite") {
+		if ($this.children('.voit__item').length > 1 && $this.attr('id') != "voite") {
 			$(ui.sender).sortable('cancel');
 		}
 	}
