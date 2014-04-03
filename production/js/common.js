32e34a5817788ea21e1c495824c4093f35b12123
mirantel <magi.mirantel@gmail.com>
2014-04-03 18:27:11 +0300

    Updated names and js. Added form.

--- production/js/common.js
+++ production/js/common.js
@@ -1,16 +1,22 @@
 $(window).load(function(){
 
+var minAge = 18;
+var minYear = new Date().getFullYear() - minAge;
+var maxYear = minYear + 100;
 
-$("#voite, .voit__cell").sortable({
+$("#js-voteData").height($("#js-voteResult").height() - 4);
+
+$("#js-voteData, .vote__cell").sortable({
 	placeholder: "ui-sortable-placeholder",
 	connectWith: ".js-sortable:not(\".full\")",
-	update: function (e, ui) {
-		if (($(this).children().length === 1) && ($(this).hasClass("voit__cell"))) {
+	update: function () {
+		if (($(this).children().length >= 1) && ($(this).hasClass("vote__cell"))) {
 			$(this).addClass("full");
 		}
+		$(this).children("input.js-votePlace").val($(this).children(".vote__item").attr("id"));
 	},
-	remove: function (e, ui) {
-		if ($(this).hasClass("voit__cell")) {
+	remove: function () {
+		if ($(this).hasClass("vote__cell")) {
 			$(this).removeClass("full");
 		}
 	},
